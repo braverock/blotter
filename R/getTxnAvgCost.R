09ab698a738ff716cbddff092d5881b065f3c1a3
Peter Carl <pcarl@gsb.uchicago.edu>
2008-12-16 03:06:37 +0000

    - Initial code commit
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/getTxnAvgCost.R
@@ -0,0 +1,32 @@
+`getTxnAvgCost` <-
+function(Portfolio, Symbol, Date)
+{ # @author Peter Carl
+
+    # DESCRIPTION:
+    # Gets the value of that period's transactions and returns the sum 
+
+    # Inputs
+    # Portfolio: a portfolio object containing transactions
+    # Symbol: an instrument identifier for a symbol included in the portfolio
+    # Date: timestamp as of which to have the most recent position
+
+    # Outputs
+    # Numeric value of the most recent position.
+
+    # FUNCTION
+    TxnData = Portfolio[[Symbol]]$txn
+    TxnValue = sum(TxnData[Date,'Txn.Avg.Cost'])
+    return(TxnValue)
+}
+
+###############################################################################
+# Blotter: Tools for transaction-oriented trading systems development
+# for R (see http://r-project.org/) 
+# Copyright (c) 2008 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id:  $
+#
+###############################################################################
