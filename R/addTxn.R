9f0fd5658e8df3a9a801d8f39a3a2724e20adb3f
Dirk Eddelbuettel <edd@debian.org>
2009-03-12 02:21:49 +0000

    add option to supply a function of Qty and Prc to set transaction costs
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/addTxn.R
+++ R/addTxn.R
@@ -15,40 +15,50 @@ function(Portfolio, Symbol, TxnDate, TxnQty, TxnPrice, TxnFees=0, verbose=TRUE)
     # TxnPrice: price at which the transaction was done
     # TxnFees: fees associated with the transaction, e.g. commissions.  Fees are
     #   indicated as positive values and will be subtracted from the transaction.
+    #   TxnFees can either be a fixed amount, or a function of two arguments
+    #   Qty and Price in which case the function is evaluated to determine the
+    #   fee amount.
 
     # Outputs:
     # Portfolio: hands back the entire portfolio object with the additional
     # transaction in the correct slot: Portfolio[[Symbol]]$txn
 
     # FUNCTION
+    # Compute transaction fees if a function was supplied
+    txncost <- ifelse( is.function(TxnFees), TxnFees(TxnQty, TxnPrice), TxnFees)
     # Calculate the value and average cost of the transaction
-    TxnValue = calcTxnValue(TxnQty, TxnPrice, TxnFees)
+    TxnValue = calcTxnValue(TxnQty, TxnPrice, txncost)
     TxnAvgCost = calcTxnAvgCost(TxnValue, TxnQty)
 
     # Calculate the change in position
     PrevPosQty = getPosQty(Portfolio, Symbol, TxnDate)
-    PosQty = PrevPosQty + TxnQty 
+    PosQty = PrevPosQty + TxnQty
 
     # Calculate the resulting position's average cost
     PrevPosAvgCost = getPosAvgCost(Portfolio, Symbol, TxnDate)
-    PosAvgCost = calcPosAvgCost(PrevPosQty, PrevPosAvgCost, TxnValue, PosQty) 
+    PosAvgCost = calcPosAvgCost(PrevPosQty, PrevPosAvgCost, TxnValue, PosQty)
 
     # Calculate any realized profit or loss (net of fees) from the transaction
     RealizedPL = calcRealizedPL(TxnQty, TxnAvgCost, PrevPosAvgCost, PosQty, PrevPosQty)
 
     # Store the transaction and calculations
-    NewTxn = xts(t(c(TxnQty, TxnPrice, TxnFees, TxnValue, TxnAvgCost, PosQty, PosAvgCost, RealizedPL)), order.by=as.POSIXct(TxnDate))
+    NewTxn = xts(t(c(TxnQty, TxnPrice, txncost, TxnValue, TxnAvgCost, PosQty, PosAvgCost, RealizedPL)), order.by=as.POSIXct(TxnDate))
     colnames(NewTxn) = c('Txn.Qty', 'Txn.Price', 'Txn.Fees', 'Txn.Value', 'Txn.Avg.Cost', 'Pos.Qty', 'Pos.Avg.Cost', 'Realized.PL')
-    Portfolio[[Symbol]]$txn <- rbind(Portfolio[[Symbol]]$txn, NewTxn) 
+    Portfolio[[Symbol]]$txn <- rbind(Portfolio[[Symbol]]$txn, NewTxn)
 
     if(verbose)
         print(paste(Symbol, TxnDate, TxnQty, "@",TxnPrice, sep=" "))
     return(Portfolio)
 }
 
+## example cost function
+pennyPerShare <- function(Qty, Price) {
+    return(Qty * 0.01)
+}
+
 ###############################################################################
 # Blotter: Tools for transaction-oriented trading systems development
-# for R (see http://r-project.org/) 
+# for R (see http://r-project.org/)
 # Copyright (c) 2008 Peter Carl and Brian G. Peterson
 #
 # This library is distributed under the terms of the GNU Public License (GPL)
