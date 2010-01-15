8af6515cae1e3cda41f6b6f425358dc0ad15dfe2
Peter Carl <pcarl@gsb.uchicago.edu>
2010-01-15 21:46:12 +0000

    - fixed ConMult math
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@166 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/calcPosAvgCost.R
+++ R/calcPosAvgCost.R
@@ -18,7 +18,7 @@ function(PrevPosQty, PrevPosAvgCost, TxnValue, PosQty, ConMult=1)
     if(PosQty == 0)
         PosAvgCost = 0
     else {
-        PosAvgCost = (PrevPosQty * PrevPosAvgCost + TxnValue)/(PosQty*ConMult)
+        PosAvgCost = (PrevPosQty * PrevPosAvgCost * ConMult + TxnValue)/(PosQty*ConMult)
     }
     return(PosAvgCost)
 }
