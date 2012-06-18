cebf9a192a60086adbc1ac1c7b07e75930456cf8
Jan Humme <jan@opentrades.nl>
2012-06-18 17:48:44 +0000

    fixed bug: Num.Trades count was one too low
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@1057 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/tradeStats.R
+++ R/tradeStats.R
@@ -92,7 +92,7 @@ tradeStats <- function(Portfolios, Symbols)
             StdTradePL <- sd(as.numeric(as.vector(PL.ne0)))   
             
             NumberOfTxns <- nrow(txn)-1
-            NumberOfTrades <- nrow(PL.ne0)-1
+            NumberOfTrades <- nrow(PL.ne0)
             
             PercentPositive <- (nrow(PL.gt0)/nrow(PL.ne0))*100
             PercentNegative <- (nrow(PL.lt0)/nrow(PL.ne0))*100
