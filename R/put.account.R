5710dea0842a61092a6a1d8ab235aefc694dc02d
Jan Humme <jan@opentrades.nl>
2013-01-12 22:46:57 +0000

    - introduced put.account() for better data abstraction
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@1352 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/put.account.R
@@ -0,0 +1,22 @@
+#' put a account object in .blotter env
+#' @param account.st string identifying account
+#' @param account account object
+#' @export
+
+put.account <- function(account.st, account)
+{
+    blotter.account.st <- paste('account', account.st, sep='.')
+    assign(blotter.account.st, account, envir=.blotter)
+}
+
+###############################################################################
+# Blotter: Tools for transaction-oriented trading systems development
+# for R (see http://r-project.org/) 
+# Copyright (c) 2008-2011 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id: getPortfolio.R 742 2011-08-25 21:12:43Z braverock $
+#
+###############################################################################
