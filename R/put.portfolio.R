9ad4f276898d71eacdf5e63b78e1f9a51591a2a5
Jan Humme <jan@opentrades.nl>
2013-01-12 22:35:57 +0000

    - introduced put.portfolio() for better data abstraction
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@1351 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/put.portfolio.R
@@ -0,0 +1,22 @@
+#' put a portfolio object in .blotter env
+#' @param portfolio.st string identifying portfolio
+#' @param portfolio portfolio object
+#' @export
+
+put.portfolio <- function(portfolio.st, portfolio)
+{
+    blotter.portfolio.st <- paste('portfolio', portfolio.st, sep='.')
+    assign(blotter.portfolio.st, portfolio, envir=.blotter)
+}
+
+###############################################################################
+# Blotter: Tools for transaction-oriented trading systems development
+# for R (see http://r-project.org/) 
+# Copyright (c) 2008-2011 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id: getPortfolio.R 742 2011-08-25 21:12:43Z braverock $
+#
+###############################################################################
