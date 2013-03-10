cd6cea892836d74bd676730d5b200ad0f798d015
Peter Carl <pcarl@gsb.uchicago.edu>
2013-03-10 20:43:32 +0000

    - fix to pass dots into chart_Series, as promised in the documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@1408 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.Posn.R
+++ R/chart.Posn.R
@@ -75,7 +75,7 @@ chart.Posn <- function(Portfolio, Symbol, Dates = NULL, ...,TA=NULL)
     # scope the Price data by Dates
     if(!is.null(Dates)) Prices=Prices[Dates]
     
-    chart_Series(Prices, name=Symbol, TA=TA)
+    chart_Series(Prices, name=Symbol, TA=TA, ...)
     if(!is.null(nrow(Buys)) && nrow(Buys) >=1 ) (add_TA(Buys,pch=2,type='p',col='green', on=1));
     if(!is.null(nrow(Sells)) && nrow(Sells) >= 1) (add_TA(Sells,pch=6,type='p',col='red', on=1));
     if(nrow(Position)>=1) {
