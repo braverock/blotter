6ccd045f1f8e39941ee983152e6c72bb8a7b42e1
Garrett See <gsee000@gmail.com>
2014-05-03 14:02:37 +0000

    make docs match usage and roxygenize, bumped to 0.8.19
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@1607 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/addAcctTxn.R
+++ R/addAcctTxn.R
@@ -2,16 +2,16 @@
 #' 
 #' For the specified Account, take in the date, amount, and type of transaction and append it to the correct list in the account object
 #' 
-#' @param name Account name, as string
+#' @param Account Account name, as string
 #' @param TxnDate  transaction date as ISO 8601, e.g., '2008-09-01' or '2010-01-05 09:54:23.12345'
 #' @param TxnType string indicating the type of account transaction, only "Addition", "Withdrawal", or "Interest" are currently supported
-#' @param amount As of now, the currency of the transaction MUST MATCH the currency of the Account.  Patches welcome.
+#' @param Amount As of now, the currency of the transaction MUST MATCH the currency of the Account.  Patches welcome.
 #' @param \dots any other passthrough parameters  
 #' @param verbose  If TRUE (default) the function prints the elements of the transaction in a line to the screen, e.g., "2007-01-08 Withdrawal 15,012,235". Suppress using FALSE.
 #' @details
 #' Adds capital transactions to a rudimentary transactions table in the Account object.  This may be useful when tracking the denominator of returns when there are changes to the account's capital or significant interest income.
 #' In the Account$summary table, there are several placeholder columns that mimic the CFTC's 13-column report.  Columns of interest here are "Additions", "Withdrawals", and "Interest".  
-#' Transactions added with this function will be added into the appropriate one of three slots in the Account object (Account$additions, Account$withdrawals, or Account$Interest), which contains an xts object of individual transactions with a date and amount.  The \code{\link{updateAcct}} function will read the transactions from each list in turn, aggregate them by the specified date scope, and slot them into the \code{Account$summary} table as it's built. \code{\link{UpdateEndEq}} should then just work.
+#' Transactions added with this function will be added into the appropriate one of three slots in the Account object (Account$additions, Account$withdrawals, or Account$Interest), which contains an xts object of individual transactions with a date and amount.  The \code{\link{updateAcct}} function will read the transactions from each list in turn, aggregate them by the specified date scope, and slot them into the \code{Account$summary} table as it's built. \code{\link{updateEndEq}} should then just work.
 #' 
 #' @seealso \code{\link{initAcct}}, \code{\link{updateAcct}}, \code{\link{updateEndEq}}
 #' @author Peter Carl
