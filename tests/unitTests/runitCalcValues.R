8fc43ab0aaa3e359020ffb3a34f8820e0b72b6e7
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2016-01-16 14:48:02 +0000

    Fix unit tests
    Some function names have changed to include a leading dot, and they are
    no longer exported, so we need to access them with the ::: operator.
    Thanks to cloudcello for the report and patch (#6284).
    
    Make transaction fees negative when calling addTxn. Change portfolio
    and account names to avoid clashing with anything a user might have
    created.
    
    Clean up objects and environments when tests exit (instead of deleting
    everything). I couldn't figure out how to do it with .setUp/.tearDown,
    so I used on.exit instead. The issue with .setUp/.tearDown was likely
    due to scoping issues, and I didn't want to assign to the .GlobalEnv.
    
    Remove test.calcRealizedPL because calcRealizedPL was removed in r411.
    Add test.calcPosAvgCost_C to test C version of .calcPosAvgCost.
    
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/blotter@1732 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/unitTests/runitCalcValues.R
+++ tests/unitTests/runitCalcValues.R
@@ -1,17 +1,18 @@
 # Author: Peter Carl, RUnit port by Ben McCann
 
 test.calcTxnValue <- function() {
-  checkEquals(99, calcTxnValue(TxnQty=10, TxnPrice=10, TxnFees=1))
+  checkEquals(99, blotter:::.calcTxnValue(TxnQty=10, TxnPrice=10, TxnFees=1))
 }
 
 test.calcTxnAvgCost <- function() {
-  checkEquals(9.9, calcTxnAvgCost(TxnValue=99, TxnQty=10))
+  checkEquals(9.9, blotter:::.calcTxnAvgCost(TxnValue=99, TxnQty=10))
 }
 
 test.calcPosAvgCost <- function() {
-  checkEquals(101, calcPosAvgCost(PrevPosQty=10, PrevPosAvgCost=100, TxnValue=1020, PosQty=20))
+  checkEquals(101, blotter:::.calcPosAvgCost(PrevPosQty=10, PrevPosAvgCost=100, TxnValue=1020, PosQty=20))
 }
 
-test.calcRealizedPL <- function() {
-  checkEquals(11.2, calcRealizedPL(TxnQty=-10, TxnAvgCost=101.1, PrevPosAvgCost=99.98, PosQty=40, PrevPosQty=50))
+test.calcPosAvgCost_C <- function() {
+  checkEquals(101, blotter:::.calcPosAvgCost_C(PrevPosQty=10, PrevPosAvgCost=100, TxnValue=1020, PosQty=20))
 }
+
--- tests/unitTests/runitUpdatePortf.R
+++ tests/unitTests/runitUpdatePortf.R
@@ -1,34 +1,37 @@
 # Author: Peter Carl, RUnit port by Ben McCann
 
-.tearDown <- function() {
-  rm(list=ls(all=TRUE, pos=.blotter), pos=.blotter)
-  rm(list=ls(all=TRUE, pos=.instrument), pos=.instrument)
-}
-
 test.txnFees <- function() {
-  data(IBM)                               # data included in package
+  on.exit({
+    # remove objects created by unit tests
+    try(rm_currencies("USD"))
+    try(rm_stocks(symbols))
+    try(rm(list=c(p1,a1), pos=.blotter))
+    try(rm(IBM))
+  })
+
   currency("USD")
   symbols <- c("IBM")
   for (symbol in symbols){
     stock(symbol, currency="USD", multiplier=1)
   }
+  data(IBM, package="blotter")
 
   ## simple portfolio with one transaction
-  p1 <- initPortf(name="p1", symbols=symbols)
-  p1 <- addTxn(Portfolio="p1", Symbol="IBM", TxnDate='2007-01-04', TxnQty=100, TxnPrice=96.5, TxnFees=0.05*100)
-  p1 <- updatePortf(Portfolio="p1", Dates='2007-01-03::2007-01-10')
-  a1 <- initAcct(name="a1", portfolios="p1")
+  p1 <- initPortf(name="p1runitUpdatePortf", symbols=symbols)
+  p1 <- addTxn(Portfolio="p1runitUpdatePortf", Symbol="IBM", TxnDate='2007-01-04', TxnQty=100, TxnPrice=96.5, TxnFees=-0.05*100)
+  p1 <- updatePortf(Portfolio="p1runitUpdatePortf", Dates='2007-01-03::2007-01-10')
+  a1 <- initAcct(name="a1runitUpdatePortf", portfolios="p1runitUpdatePortf")
   a1 <- updateAcct(a1,'2007-01')
   a1 <- updateEndEq(a1,'2007-01')
 
   ## (really) simple transaction cost function
-  fiveCents <- function(qty, prc) return(0.05*qty)
-  p2 <- initPortf(name="p2", symbols=symbols)
-  p2 <- addTxn(Portfolio="p2", Symbol="IBM", TxnDate='2007-01-04', TxnQty=100, TxnPrice=96.5, TxnFees=fiveCents)
-  p2 <- updatePortf(Portfolio="p2", Dates='2007-01-03::2007-01-10')
-  a2 <- initAcct(name="a2", portfolios="p2")
+  fiveCents <- function(qty, prc, ...) return(-0.05*qty)
+  p2 <- initPortf(name="p2runitUpdatePortf", symbols=symbols)
+  p2 <- addTxn(Portfolio="p2runitUpdatePortf", Symbol="IBM", TxnDate='2007-01-04', TxnQty=100, TxnPrice=96.5, TxnFees=fiveCents)
+  p2 <- updatePortf(Portfolio="p2runitUpdatePortf", Dates='2007-01-03::2007-01-10')
+  a2 <- initAcct(name="a2runitUpdatePortf", portfolios="p2runitUpdatePortf")
   a2 <- updateAcct(a2,'2007-01')
   a2 <- updateEndEq(a2,'2007-01')
 
-  checkEquals(getAccount('a1')$summary$End.Eq, getAccount('a2')$summary$End.Eq)
+  checkEquals(getAccount(a1)$summary$End.Eq, getAccount(a2)$summary$End.Eq)
 }
