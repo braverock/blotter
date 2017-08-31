c107048dd489002f9b16610f5e683a4d31b5da8d
Jasen Mackie <jaymon0703@gmail.com>
2017-08-31 21:25:53 +0200

    First complete draft for 10-Index Adjusted Performnce Metric

--- R/IndexAdjustedCost.R
+++ R/IndexAdjustedCost.R
@@ -3,7 +3,8 @@
 #' The Arrival Cost (or Trading Cost) is the difference between the
 #' average execution price and the price of the asset at the time the
 #' order was entered into the market, ie. the Arrival Price.
-#'
+#' 
+#' Positive means a cost, negative means a saving.
 #'
 #' @param Portfolio A portfolio name that points to a portfolio object structured with initPortf()
 #' @param Symbol An instrument identifier for a symbol included in the portfolio, e.g., "IBM"
@@ -37,38 +38,58 @@ ArrivalCost <- function(Portfolio, Symbol, side="Buy")
 }
 
 #' Index Cost
+#' 
+#' The Index Cost is the difference between the
+#' average execution price and the price of the Index (or ETF) at the time the
+#' order was entered into the market, ie. the Arrival Price.
+#' 
+#' To get the index VWAP of one does not have the underlying trades and weightings,
+#' a commonly used proxy is an ETF tracking the relevant index.
+#' 
+#' Positive means a cost, negative means a saving.
 #'
-#' @param IndexVWAP value representing the benchmark VWAP
-#' @param IndexArrivalCost value representing the benchmark arrival price
+#' @param Portfolio A portfolio name that points to a portfolio object structured with initPortf()
+#' @param IndexSymbol An instrument identifier for an index used for adjusting a stock's Arrival Cost (typicall ETF)
 #'
 #' @return
+#' 
+#' A numeric value in basis points
+#' 
 #' @export
 #'
 #' @examples
 #' 
-IndexCost <- function(IndexVWAP, IndexArrivalCost){
-  ic <- ((IndexVWAP - IndexArrivalCost)/IndexArrivalCost)*10000
+IndexCost <- function(Portfolio, IndexSymbol){
+  pname <- Portfolio
+  #If there is no table for the symbol then create a new one
+  if(is.null(.getPortfolio(pname)$symbols[[IndexSymbol]]))
+    addPortfInstr(Portfolio=pname, symbols=IndexSymbol)
+  Portfolio <- .getPortfolio(pname)
+  txns <- Portfolio$symbols[[IndexSymbol]]$txn
+  p_avg <- as.numeric(last(txns$Pos.Avg.Cost))
+  p_0 <- as.numeric(txns$Pos.Avg.Cost[min(which(txns != 0))])
+  ic <- ((p_avg - p_0)/p_0)*10000
   ic
 }
 
-#'  Index Adjusted Cost
+#' Index Adjusted Cost
+#' 
+#' A market or index adjusted metric accounting for price trend in a stock
+#' due to the overall market.
 #'
 #' @param ArrivalCost value representing arrival cost
 #' @param IndexCost value representing index cost
-#' @param beta value represent underlying stock beta to index
+#' @param beta value representing stock K's sensitivity to the index
 #'
 #' @return
+#' 
+#' The Index Adjusted Cost
+#' 
 #' @export
 #'
 #' @examples
 #' 
 IndexAdjustedCost <- function(ArrivalCost, IndexCost, beta){
-  iac <- ArrivalCost - beta * IndexCost
+  iac <- ArrivalCost - (beta * IndexCost)
   cat(iac,"bps")
 }
-
-# data <- read.csv("C:/Users/jasen/Personal/GitHub/31_trades_history")
-# #txns <- xts(cbind(data$UNDERLYING_QUANTITY[7:48], data$UNDERLYING_PRICE[7:48]/100),as.POSIXct(data$TRADE_DATE[7:48]))
-# #write.csv(txns, "C:/Users/jasen/Personal/GitHub/sample_txns.csv")
-# txns <- read.csv("C:/Users/jasen/Personal/GitHub/blotter/demo/sample_txns.csv")
-# 
