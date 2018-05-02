braverock/blotter

commit bf4348885939bea72ab8d59ecefc607f585758e0

Adding a condition for determining when to layer
In order to layer, we need to determine the index of the long and short duration dataframes (longdf & shortdf respectively) which take us over the duration of the first layer. If the last index before our target long and short durations is the final row in the long and short duration dataframes, we should not proceed to layering.

At present, we only check to see if the targetlongrow or targetshortrow == 0 when determining whether or not to proceed. I am adding another condition which checks that targetlongrow and targetshortrow > number of rows in longdf and shortdf before proceeding.

We could implement a solution higher up when targetlongrow and targetshortrow are first created, and render targetlongrow and/or targetshortrow == 0 if this is the case, but it feels more appropriate to add a condition to one which already exists for determining whether or not to proceed with layering.

R/txnsim.R
@@ -513,7 +513,7 @@ txnsim <- function(Portfolio,
         )
 
         # get the range and number of rows remaining of long and short trades
-        if(targetshortrow != 0){ # ie. there are short round turn trades in the strategy
+        if(targetshortrow != 0 & targetshortrow > nrow(shortdf)){ # ie. there are short round turn trades in the strategy
           shortrange <- (targetshortrow+1):nrow(shortdf)
           nshort     <- length(shortrange)
         } else {
@@ -521,7 +521,7 @@ txnsim <- function(Portfolio,
           nshort <- 0
         }
         # nshort     <- length(shortrange)
-        if(targetlongrow != 0){ # ie. there are long round turn trades in the strategy
+        if(targetlongrow != 0 & targetlongrow > nrow(longdf)){ # ie. there are long round turn trades in the strategy
           longrange  <- (targetlongrow+1):nrow(longdf)
           nlong      <- length(longrange)
         } else {

commit aa937a9c51603146a8be6866e3e1b7fbd5b51e31

Fix vectorised implementation for building txns, and fix bug related to incorrectly facing operator
The vectorised implementation for building our txns in txnsim.txns() uses findInterval() to assign timestamps and prices to our txns. When our original observed strategy is a strategy with layering, the vectorised solution implemented with commit c172724 introduces a bug, in that findInterval() will be called with an unordered input parameter (the layered trades with older timestamps will come after the more recent timestamps in the time series). To fix, we order the df before passing to findInterval().

Also, commit bf43488 introduced a bug in that layering was never possible with the operator the wrong way round. We switch the operator to be smaller than (<) nrow(shortdf) and nrow(longdf) to fix. This will be true when layering is required and will allow the algorithm to proceed to layering.

See #68
Fixes #68

R/txnsim.R
@@ -513,7 +513,7 @@ txnsim <- function(Portfolio,
         )
 
         # get the range and number of rows remaining of long and short trades
-        if(targetshortrow != 0 & targetshortrow > nrow(shortdf)){ # ie. there are short round turn trades in the strategy
+        if(targetshortrow != 0 & targetshortrow < nrow(shortdf)){ # ie. there are short round turn trades in the strategy
           shortrange <- (targetshortrow+1):nrow(shortdf)
           nshort     <- length(shortrange)
         } else {
@@ -521,7 +521,7 @@ txnsim <- function(Portfolio,
           nshort <- 0
         }
         # nshort     <- length(shortrange)
-        if(targetlongrow != 0 & targetlongrow > nrow(longdf)){ # ie. there are long round turn trades in the strategy
+        if(targetlongrow != 0 & targetlongrow < nrow(longdf)){ # ie. there are long round turn trades in the strategy
           longrange  <- (targetlongrow+1):nrow(longdf)
           nlong      <- length(longrange)
         } else {
@@ -1052,6 +1052,7 @@ txnsim.txns <- function (reps, Portfolio, replacement, n, ...) {
         df <- df[which(df$duration != 0), ] # remove zero duration trades
         # Build opening transaction dataframe
         txns_open <- data.frame(matrix(nrow = nrow(df), ncol = 3))
+        df <- df[order(df[,1]),] # when layering, we have older order timestamps from layer 2+ after more recent timestamps from layer 1
         idx_open <- findInterval(df[,1],index(prices))
         txns_open[,1] <- index(prices)[idx_open]
         txns_open[,2] <- df[, "quantity"]