braverock/blotter

commit b617b04444ee797342a7d7187990aba562e6b656

Add support to mcsim for multivariate series, where replacement=FALSE
see #91

R/mcsim.R
@@ -269,13 +269,54 @@ mcsim <- function(  Portfolio = NULL
           s <- seq(sample.int(l,1),length(x),by=l)
         }
         blocks<-split(x, findInterval(x,s))
+        
+        if(!is.null(aggregateFUN) && !is.function(aggregateFUN)) {
+          if(exists(aggregateFUN, mode="function")) {
+            fn <- get(aggregateFUN, mode="function")
+          } else {
+            stop("Cannot aggregate with ", aggregateFUN," because there is no function by that name to call")
+          }
+        } else {
+          fn <- aggregateFUN
+        }
+        
         # now reassemble the target index order
         idx <- unlist(blocks[sample(names(blocks),size = length(blocks),replace = FALSE)]) ; names(idx)<-NULL
-        tmp <- as.vector(dailyPL)[idx]
+          if(ncol(dailyPL)==1){
+            tmp <- cbind(tmp, EndEqdf[idx,])
+          } else {
+            if(!is.null(aggregateFUN)){
+              tmp <- xts(coredata(dailyPL)[idx],index(dailyPL))
+              tmp <- coredata(fn(tmp,...))
+              # tmp <- cbind(tmp,tmpseries)
+            }
+          }
+        # tmp <- as.vector(dailyPL)[idx]
         
       } else {
         # block length is 1, just sample with or without replacement
-        tmp <- sample(as.vector(dailyPL), replace = replacement)
+        idx <- sample(nrow(dailyPL), replace = replacement) ; names(idx)<-NULL
+        
+        if(!is.null(aggregateFUN) && !is.function(aggregateFUN)) {
+          if(exists(aggregateFUN, mode="function")) {
+            fn <- get(aggregateFUN, mode="function")
+          } else {
+            stop("Cannot aggregate with ", aggregateFUN," because there is no function by that name to call")
+          }
+        } else {
+          fn <- aggregateFUN
+        }
+        
+          if(ncol(dailyPL)==1){
+            tmp <- cbind(tmp, EndEqdf[idx,])
+          } else {
+            if(!is.null(aggregateFUN)){
+              tmp <- xts(coredata(dailyPL)[idx],index(dailyPL))
+              tmp <- coredata(fn(tmp,...))
+              # tmp <- cbind(tmp,tmpseries)
+            }
+          }
+        # tmp <- sample(as.vector(dailyPL), replace = replacement)
       }
       tsbootxts <- xts(tmp, index(dailyPL))
     }

commit d096b686d30bb63ebf0cbc2af7ef84b88b5c4820

Fix replacement=FALSE bug introduced with b617b04
Basically, univariate support broke with the previous commit

see #91

R/mcsim.R
@@ -257,7 +257,8 @@ mcsim <- function(  Portfolio = NULL
       if(isTRUE(l>1)) {
         # do a block resample, without replacement
         # first, resample the index
-        x <- 1:length(dailyPL)
+        # x <- 1:length(dailyPL)
+        x <- 1:nrow(dailyPL)
         # now sample blocks
         if (isTRUE(varblock)){
           # this method creates variable length blocks with a uniform random
@@ -284,7 +285,7 @@ mcsim <- function(  Portfolio = NULL
         # now reassemble the target index order
         idx <- unlist(blocks[sample(names(blocks),size = length(blocks),replace = FALSE)]) ; names(idx)<-NULL
           if(ncol(dailyPL)==1){
-            tmp <- cbind(tmp, EndEqdf[idx,])
+            tmp <- EndEqdf[idx,]
           } else {
             if(!is.null(aggregateFUN)){
               tmp <- xts(coredata(dailyPL)[idx],index(dailyPL))
@@ -309,7 +310,7 @@ mcsim <- function(  Portfolio = NULL
         }
         
           if(ncol(dailyPL)==1){
-            tmp <- cbind(tmp, EndEqdf[idx,])
+            tmp <- EndEqdf[idx,]
           } else {
             if(!is.null(aggregateFUN)){
               tmp <- xts(coredata(dailyPL)[idx],index(dailyPL))