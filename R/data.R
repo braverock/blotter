1dc44e5e2f469f3869260f836ed202d11a91d1fb
Jasen Mackie <jaymon0703@gmail.com>
2019-06-02 20:05:13 +0000

    Add some random market data for testing TCA project for GSoC 2019
    See #54

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Prices of random security ABC for a full trading day.
+#'
+#' To be used for among other things testing the TCA models being
+#' added to blotter as part of GSoC 2019 by Vito Lestingi.
+#'
+#' @format An xts object with 163,936 rows and 2 variables:
+#' \describe{
+#'   \item{TradePrice}{price in cents}
+#'   \item{TradeVolume}{traded volume}
+#'   ...
+#' }
+#' @source \url{}
+"ABC"
