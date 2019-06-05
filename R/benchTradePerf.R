0d4ee1ccec78200b573da4bc019af85bde88c605
Vito Lestingi <LestingiV@gmail.com>
2019-06-05 10:52:26 +0200

    Update benchTradePerf() docs
    - fix some typos
    - remove full stops where requested
    - refine some points
    - add some TODOs where formulas are needed

--- R/benchTradePerf.R
+++ R/benchTradePerf.R
@@ -1,37 +1,51 @@
 #' Trade Execution Performance Benchmarks
 #' 
-#' This function gathers different benchmarking methods used to evaluate the average execution price \eqn{\bar{P}} of a given trading strategy.
-#' The \eqn{\bar{P}} is compared against a number of benchmark metrics, in order to assess its performance in terms of profit or loss.
-#' These methods are not strictly alternative, indeed each of them provides different insights and suffers limitations.
+#' This function gathers different benchmarking methods used to evaluate the 
+#' average execution price \eqn{\bar{P}} of a given trading strategy.
+#' The \eqn{\bar{P}} is compared against a number of benchmark metrics, in order
+#' to assess its performance in terms of profit or loss relative to a given benchmark.
+#' These benchmarks are not mutually exclusive, each of them provides different insights
+#' and may have shortcomings. They can be used in conjuction to account for this aspects.
 #' 
 #' The performance is quantified by means of a \emph{Profit and Loss (PnL) metric}. 
-#' A positive PnL metric indicates that the trading strategy outperformed a chosen benchmark on average,
-#' viceversa negative values register an underperformance.
+#' A positive PnL metric indicates that the trading strategy outperformed a chosen 
+#' benchmark on average, vice versa negative values register an underperformance.
 #' 
 #' By and large, PnL metrics are computed as:
 #' 
+#' #TODO
 #' 
-#' where \eqn{P_{avg}} is the average execution price and \eqn{P_{B}} is a given benchmark price that can be a single next/present open/close price.
+#' where \eqn{P_{avg}} is the average execution price and \eqn{P_{B}} is a given 
+#' benchmark price that can be a single next/present open/close price.
 #' It is worth stressing that they are expressed in basis points (bps) units.
 #' 
-#' A widely used one is the \emph{Volume Weighted Average Price (VWAP) benchamark}... 
+#' A widely used one is the \emph{Volume Weighted Average Price (VWAP) benchamark}.
+#' Where the VWAP benchmark is:
+#' 
+#' #TODO
+#' 
+#' It may vary by timespan considered and data vendors.
 #' 
 #' A variation of the VWAP benchmark is given by the \emph{Participation Weighted Price (PWP) benchmark}.
+#' Where the weighting is with respect to the \emph{PWP shares}:
+#'   
+#' #TODO
 #' 
+#' Lastly the \emph{Relative Performance Measure}, which differs from the PnL metrics above. 
+#' It is expressed as:
 #' 
-#' Lastly the \emph{Relative Performance Measure}, which differs from the PnL metrics above. It is expressed as:
-#' 
+#' #TODO
 #' 
 #' 
 #' @param Portfolio A portfolio name that points to a portfolio object structured with initPortf()
 #' @param Symbol A string identifying the traded symbol to benchmark
-#' @param side Numeric, either 1 or -1, that indicates the side of the trade. \code{side = 1} means "Buy" and \code{side = -1} is "Sell". Default is \code{side = 1}.
-#' @param benchmark A string or vector of strings providing the benchmark metric(s). One of 'MktBench', 'VWAP', 'PWP', 'RPM'. Default is #TODO.
-#' @param type A string specifying the type of the benchmark used. Relevant only for \code{benchmark = 'MktBench'} and \code{benchmark = 'VWAP'}. 
+#' @param side A numeric value, that indicates the side of the trade. Either 1 or -1, \code{side = 1} (default) means "Buy" and \code{side = -1} is "Sell"
+#' @param benchmark A string or vector of strings providing the one or several of the 'MktBench', 'VWAP', 'PWP' or 'RPM' benchmark metrics. Default is #TODO
+#' @param type A string specifying the type of the benchmark used. Relevant only for \code{benchmark = 'MktBench'} and \code{benchmark = 'VWAP'}
 #'             In the former case it only changes the output format, while in the latter it also specifies the VWAP to be used. If \code{benchmark = 'VWAP'}, default is \code{type = 'Txns'}
-#' @param MktData An xts object containing a 'MktPrice' and 'MktVolmn' required columns.
-#' @param POV A number between 0 and 1, specifying the POV rate.
-#' @param verbose A logical. It allows a RPM qualitative score appended. Default is \code{FALSE}.
+#' @param MktData An xts object containing a 'MktPrice' and 'MktVolmn' required columns
+#' @param POV A numeric value between 0 and 1, specifying the POV rate
+#' @param verbose A logical value. It allows a RPM qualitative score appended. Default is \code{FALSE}
 #'
 #' 
 #' @return 
@@ -41,30 +55,30 @@
 #' For \code{benchmark = 'MktBench'} it contains:
 #' \describe{
 #'      \item{\code{Symbol}: }{A string identifying the traded symbol to benchmark}
-#'      \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell".}
+#'      \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell"}
 #'      \item{\code{Avg.Exec.Price}: }{Symbol transactions average execution price}
-#'      \item{\code{MktBench.*}: }{The benchmark and an arbitrary \code{type} provided as input (e.g. 'Open', 'Close').}
-#'      \item{\code{Performance}: }{The \emph{Benchmark PnL} performance, in bps.}
+#'      \item{\code{MktBench.*}: }{The benchmark and an arbitrary \code{type} provided as input (e.g. 'Open', 'Close')}
+#'      \item{\code{Performance}: }{The \emph{Benchmark PnL} performance, in bps}
 #' }
 #' 
 #' For \code{benchmark = 'VWAP'} it contains:
 #' \describe{
 #'      \item{\code{Symbol}: }{A string identifying the traded symbol to benchmark}
-#'      \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell".}
+#'      \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell"}
 #'      \item{\code{Avg.Exec.Price}: }{Symbol transactions average execution price}
-#'      \item{\code{VWAP.*}: }{The benchmark and depending on \code{type} parameter either 'Txns' 'Mkt'.}
-#'      \item{\code{Performance}: }{The \emph{VWAP PnL} metric, in bps.}
+#'      \item{\code{VWAP.*}: }{The benchmark and depending on \code{type} parameter either 'Txns' 'Mkt'}
+#'      \item{\code{Performance}: }{The \emph{VWAP PnL} metric, in bps}
 #' }
 #' 
 #' For \code{benchmark = 'PWP'} it contains:
 #' \describe{
 #'      \item{\code{Symbol}: }{A string identifying the traded symbol to benchmark}
-#'      ### \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell".}
+#'      \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell"}
 #'      \item{\code{Avg.Exec.Price}: }{Symbol transactions average execution price}
 #'      \item{\code{POV}: }{The POV rate of the order}
 #'      \item{\code{PWP.Shares}: }{The ratio between the total unit traded and the POV rate}
 #'      \item{\code{PWP.Price}: }{Volume weighted price of the first \code{PWP.Shares} traded}
-#'      \item{\code{Performance}: }{The \emph{PWP PnL} metric, in bps.}
+#'      \item{\code{Performance}: }{The \emph{PWP PnL} metric, in bps}
 #' }
 #' 
 #' For \code{benchmark = 'RPM'} it contains:
@@ -72,11 +86,11 @@
 #'      \item{\code{Symbol}: }{A string identifying the traded symbol to benchmark}
 #'      \item{\code{Side}: }{The \code{side} of the trades, as "Buy" or "Sell".}
 #'      \item{\code{Avg.Exec.Price}: }{Symbol transactions average execution price}
-#'      \item{\code{t.Mkt.Volmn}: }{Total market volume over the order timespan.}
-#'      \item{\code{t.Fav.Volmn}: }{Total market volume over the order timespan for which the average execution price of a 'Buy' ('Sell') order was lower (greater) then market prices.}
-#'      \item{\code{t.Unfav.Volmn}: }{The opposite of \code{t.Fav.Volmn}.}
+#'      \item{\code{t.Mkt.Volmn}: }{Total market volume over the order timespan}
+#'      \item{\code{t.Fav.Volmn}: }{Total market volume over the order timespan for which the average execution price of a 'Buy' ('Sell') order was lower (greater) than market prices}
+#'      \item{\code{t.Unfav.Volmn}: }{The opposite of \code{t.Fav.Volmn}}
 #'      \item{\code{RPM}: }{The \emph{relative performance measure}. Decimal in the 0 to 1 range.}
-#'      \item{\code{Quality}: }{A qualitiative score of the RPM over over quintiles, bottom-up one of 'Poor', 'Fair', 'Average', 'Good', 'Excellent'. Present if \code{verbose = TRUE}.}
+#'      \item{\code{Quality}: }{A qualitiative RPM score over quintiles, bottom-up one of 'Poor', 'Fair', 'Average', 'Good', 'Excellent'. Present if \code{verbose = TRUE}}
 #' }
 #' 
 #' @seealso \code{\link{initPortf}}, \code{\link{addTxn}}
