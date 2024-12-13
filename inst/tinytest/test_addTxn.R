f12c325ec24c40042655f48d8ac366d93cc2c04e
JustinMShea <justin.shea@gmail.com>
2024-12-13 14:47:51 -0600

    removed 'try' wrapper so tests run, removed 'silent'and added 'verbose=TRUE', removed forloop on 1 security in test_addTxn.R, and 'updated_index_attributes' on amzn data.

--- inst/tinytest/test_addTxn.R
+++ inst/tinytest/test_addTxn.R
@@ -1,31 +1,31 @@
-# Author: Peter Carl -> RUnit port by Ben McCann -> 
-# -> tinytest port by Justin Shea
+# Author: Peter Carl -> RUnit port by Ben McCann -> tinytest port by Justin Shea
 
 library(tinytest)
 
 test_addTxn <- function() {
-  try({
+
     currency("USD")
-    symbols <- c("IBM")
-    for (symbol in symbols) {
-      stock(symbol, currency = "USD", multiplier = 1)
-    }
+    symbol <- c("IBM")
+  
+    stock(symbol, currency = "USD", multiplier = 1)
+
     data(IBM, package = "blotter")
+    IBM <- xts:::.update_index_attributes(IBM)
     
     # Initialize a portfolio object
-    p <- initPortf("runitAddTxn", symbols = symbols)
+    p <- initPortf("runitAddTxn", symbols = symbol)
     
     # Add transactions
-    addTxn(p, "IBM", '2007-01-03',  50,  96.5,  TxnFees = -0.05 * 50)
-    addTxn(p, "IBM", '2007-01-04', -50,  97.1,  TxnFees = -0.05 * 50)
-    addTxn(p, "IBM", '2007-01-08', -10,  99.2,  TxnFees = -0.05 * 10)
-    addTxn(p, "IBM", '2007-01-09', -10, 100.1,  TxnFees = -0.05 * 10)
-    addTxn(p, "IBM", '2007-01-17', -10, 100.25, TxnFees = -0.05 * 10)
-    addTxn(p, "IBM", '2007-01-19',  30,  95,    TxnFees = -0.05 * 30)
-    addTxn(p, "IBM", '2007-01-22',  25,  96.3,  TxnFees = -0.05 * 25)
-    addTxn(p, "IBM", '2007-01-23',  25,  96.42, TxnFees = -0.05 * 25)
-    addTxn(p, "IBM", '2007-01-26', -25,  97.52, TxnFees = -0.05 * 25)
-    addTxn(p, "IBM", '2007-01-31', -25,  98.80, TxnFees = -0.05 * 25)
+addTxn(p, "IBM", '2007-01-03',  50,  96.5,  TxnFees = -0.05 * 50, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-04', -50,  97.1,  TxnFees = -0.05 * 50, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-08', -10,  99.2,  TxnFees = -0.05 * 10, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-09', -10, 100.1,  TxnFees = -0.05 * 10, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-17', -10, 100.25, TxnFees = -0.05 * 10, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-19',  30,  95,    TxnFees = -0.05 * 30, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-22',  25,  96.3,  TxnFees = -0.05 * 25, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-23',  25,  96.42, TxnFees = -0.05 * 25, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-26', -25,  97.52, TxnFees = -0.05 * 25, verbose=FALSE)
+addTxn(p, "IBM", '2007-01-31', -25,  98.80, TxnFees = -0.05 * 25, verbose=FALSE)
     
     portfolio <- getPortfolio(p)
     transactions <- portfolio$symbols[["IBM"]]$txn
@@ -33,7 +33,6 @@ test_addTxn <- function() {
     expect_equal(sum(transactions$Txn.Fees), -13)
     expect_equal(sum(transactions$Txn.Qty), 0)
     
-  }, silent = TRUE)
 }
 
 test_addTxn()
--- inst/tinytest/test_addTxns.R
+++ inst/tinytest/test_addTxns.R
@@ -8,7 +8,12 @@ library(tinytest)
 test_addTxns <- function() {
   try({
     data("amzn", package = "blotter")
+    
+    amzn <- xts:::.update_index_attributes(amzn)
+    amzn.trades <- xts:::.update_index_attributes(amzn.trades)
+    
     amzn.trades <- merge(amzn.trades, TxnFees = -0.1)
+    
     currency("USD")
     stock("AMZN", currency = "USD", multiplier = 1)
     
@@ -16,17 +21,19 @@ test_addTxns <- function() {
     initPortf("amzn_txn2", symbols = "AMZN")
     initPortf("amzn_txns2", symbols = "AMZN")
     
-    addTxns("amzn_txns2", "AMZN", TxnData = amzn.trades)
+    addTxns("amzn_txns2", "AMZN", TxnData = amzn.trades, verbose=FALSE)
     
     for (i in 1:nrow(amzn.trades)) {
       TxnTime <- index(amzn.trades)[i]
       amzn.trade <- data.frame(amzn.trades[i,])
-      with(amzn.trade, addTxn("amzn_txn2", "AMZN", TxnTime, TxnQty, TxnPrice, TxnFees = TxnFees))
+      with(amzn.trade, addTxn("amzn_txn2", "AMZN", TxnTime, TxnQty, TxnPrice, TxnFees = TxnFees, verbose = FALSE ))
     }
     
     t1 <- getPortfolio("amzn_txn2")$symbols$AMZN$txn
     t2 <- getPortfolio("amzn_txns2")$symbols$AMZN$txn
+    
     expect_identical(t1, t2)
+    
     }, silent = TRUE)
 }
 
