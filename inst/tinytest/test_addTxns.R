ccbe3a94e7bbca50bc5bc52f050fa1765490ae84
JustinMShea <justin.shea@gmail.com>
2024-12-13 22:54:16 +0000

    fixed remaining 'warnings' in test files related to xts tz issues.

--- inst/tinytest/test_addTxns.R
+++ inst/tinytest/test_addTxns.R
@@ -1,40 +1,36 @@
-# Author: Peter Carl -> RUnit port by Ben McCann -> 
-# -> tinytest port by Justin Shea
-
+# Author: Peter Carl -> RUnit port by Ben McCann -> tinytest port by Justin Shea
 library(tinytest)
-
-.blotter <- new.env()
+Sys.setenv(TZ='GMT')
 
 test_addTxns <- function() {
-  try({
+
+    currency("USD")
+    
     data("amzn", package = "blotter")
-    
-    amzn <- xts:::.update_index_attributes(amzn)
-    amzn.trades <- xts:::.update_index_attributes(amzn.trades)
-    
+
     amzn.trades <- merge(amzn.trades, TxnFees = -0.1)
     
-    currency("USD")
-    stock("AMZN", currency = "USD", multiplier = 1)
+    stock("amzn", currency = "USD", multiplier = 1)
     
     initAcct("amzn_acct2", portfolios = c("amzn_txn2", "amzn_txns2"), initEq = 10000)
-    initPortf("amzn_txn2", symbols = "AMZN")
-    initPortf("amzn_txns2", symbols = "AMZN")
+    initPortf("amzn_txn2", symbols = "amzn")
+    initPortf("amzn_txns2", symbols = "amzn")
     
-    addTxns("amzn_txns2", "AMZN", TxnData = amzn.trades, verbose=FALSE)
+    addTxns("amzn_txns2", "amzn", TxnData = amzn.trades, verbose=FALSE)
     
     for (i in 1:nrow(amzn.trades)) {
       TxnTime <- index(amzn.trades)[i]
       amzn.trade <- data.frame(amzn.trades[i,])
-      with(amzn.trade, addTxn("amzn_txn2", "AMZN", TxnTime, TxnQty, TxnPrice, TxnFees = TxnFees, verbose = FALSE ))
+      with(amzn.trade, addTxn("amzn_txn2", "amzn", TxnTime, TxnQty, TxnPrice, TxnFees = TxnFees, verbose = FALSE ))
     }
     
-    t1 <- getPortfolio("amzn_txn2")$symbols$AMZN$txn
-    t2 <- getPortfolio("amzn_txns2")$symbols$AMZN$txn
+    t1 <- getPortfolio("amzn_txn2")$symbols$amzn$txn
+    t2 <- getPortfolio("amzn_txns2")$symbols$amzn$txn
     
     expect_identical(t1, t2)
-    
-    }, silent = TRUE)
+
 }
 
 test_addTxns()
+
+
